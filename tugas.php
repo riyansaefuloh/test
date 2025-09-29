f807071225306682faba49338dc17debad8ba08e
riyansaefuloh <riyansaefuloh123@gmail.com>
2025-09-29 13:30:56 +0700

    menambahkan data diri link ig link facebook

--- tugas.php
+++ tugas.php
@@ -4,9 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perkenalan</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body>
-    <img src="profile.png" alt="profile">
-    
+    <div class="container">
+
+        <div class="header">
+            <h1>Welcome in personal web</h1>
+        </div>
+
+        <div class="profile">
+            <img src="profile.png" alt="profile">
+        </div>
+
+        <div class="data-diri">
+            <?php
+            $nama ="Riyan saefuloh";
+            $nim ="102092400033";
+            $fakultas="Rekayasa Industri";
+            $prodi ="Sistem Informasi";
+            echo" $nama/$nim/$fakultas/$prodi";
+            ?>
+        </div>
+   
+        <div class="sosmed">
+            <a href="https://www.facebook.com/share/1K18WjAHSh/">
+                <img src="facebook.png" alt="facebook">
+            </a>
+            <a href="https://www.instagram.com/riyan.pxr_?igsh=MXBkZjE0YWloamF1aQ==">
+                <img src="instagram.png" alt="instagram">
+            </a>
+        </div>
+
+
+    </div>
 </body>
 </html>
