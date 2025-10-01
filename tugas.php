5f2a61d18e806182deea35fddc67071ad7743807
riyansaefuloh <riyansaefuloh123@gmail.com>
2025-10-02 00:26:15 +0700

    penyelesaian tugas

--- tugas.php
+++ tugas.php
@@ -4,46 +4,66 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perkenalan</title>
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="container">
-
-        <div class="header">
-            <h1>Welcome in personal web</h1>
+    <div class="container"> 
         </div>
 
         <div class="profile">
-            <img src="profile.jpg" alt="profile" width="100" height="100">
+            <img src="profile.jpg" alt="profile">
         </div>
 
         <div class="data-diri">
-            <?php
+            <h2> <?php
             $nama ="Riyan saefuloh";
             $nim ="102092400033";
             $fakultas="Rekayasa Industri";
             $prodi ="Sistem Informasi";
             echo" $nama/$nim/$fakultas/$prodi";
-            ?>
+            ?></h2>
         </div>
    
         <div class="sosmed">
-            <table>
-                <tr>
-                    <td align="center">
-                        <a href="https://www.facebook.com/share/1K18WjAHSh/">
-                            <img src="facebook.png" alt="facebook" width="50" height="50"><br>
-                             facebook
+        
+                        <a class="sosmed_a" href="https://www.facebook.com/share/1K18WjAHSh/" target="blank">
+                            <img class="img" src="facebook.png" alt="facebook">
+                            <span>facebook</span>
                         </a>
-                    </td>
-                    <td align="center">
-                        <a href="https://www.instagram.com/riyan.pxr_?igsh=MXBkZjE0YWloamF1aQ==">
-                            <img src="instagram.png" alt="instagram" width="50" height="50"><br>
-                             instagram
+                    
+                        <a class="sosmed_a" href="https://www.instagram.com/riyan.pxr_?igsh=MXBkZjE0YWloamF1aQ==" target="blank">
+                            <img class="img" src="instagram.png" alt="instagram">
+                            <span>instagram</span>
                         </a>
-                    </td>
-                </tr>
-            </table>
+
+                        <a class="sosmed_a" href="https://www.linkedin.com/in/riyan-saefuloh-b671b6328?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"target="blank">
+                            <img class="img" src="linkedIn.png" alt="linkedIn">
+                            <span>linkedIn</span>
+                        </a>
+        
+        <table class="table">
+        <tr>
+        <td><b>Nama</b><br><?php echo $nama ?></td>
+        </tr>
+        <tr>
+        <td><b>Nim</b><br><?php echo $nim ?></td>
+        </tr>
+        <tr>
+        <td><b>Fakultas</b><br><?php echo $fakultas ?></td>
+        </tr>
+        <tr>
+        <td><b>Prodi</b><br><?php echo $prodi ?></td>
+        </tr>
+        </table>
+                  
+        </div>
+        <div class="footer">
+            <?php
+            date_default_timezone_set(timezoneId: "Asia/Jakarta");
+            echo "Sekarang: " . date(format: "l, d-m-Y H:i:s")."<br>";
+            echo "Dibuat dengan sepenuh hati";
+            ?>
         </div>
     </div>
 </body>
